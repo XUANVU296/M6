12b3dd1a6a6e9a40bf14b7dabf5b162fd985a109
baole123123 <lechibaovlog@gmail.com>
2024-03-11 08:29:10 +0700

    a

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/products', [ProductController::class, 'index'])->name('index');
-Route::get('/products/{id}', [ProductController::class, 'show'])->name('show');
-
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
